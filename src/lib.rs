66eefabe41f98269c111afe89f42a22f4a61a32a
InDieTasten <indietasten@gmail.com>
2023-10-14 15:37:28 +0200

    Spawn 2d camera and top level game state

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,30 @@
 use bevy::app::App;
 use bevy::prelude::*;
 
+#[cfg(debug_assertions)]
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+
+#[derive(States, Default, Clone, Eq, PartialEq, Debug, Hash)]
+enum GameState {
+    #[default]
+    Loading,
+    Playing,
+}
+
 pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
-    fn build(&self, _app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_state::<GameState>()
+            .add_systems(OnEnter(GameState::Playing), spawn_camera);
+
+        #[cfg(debug_assertions)]
+        {
+            app.add_plugins((FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin::default()));
+        }
+    }
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
