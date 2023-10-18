a709cd0d78eed0b3acec8013f2fac447d86730d7
InDieTasten <indietasten@gmail.com>
2023-10-18 05:43:35 +0200

    Add scene root component as parent for scene entities

--- src/scenario1/components.rs
+++ src/scenario1/components.rs
@@ -2,3 +2,6 @@ use bevy::prelude::*;
 
 #[derive(Component)]
 pub struct PlanetEvacuationVessel;
+
+#[derive(Component)]
+pub struct SurfaceSceneRoot;
--- src/scenario1/mod.rs
+++ src/scenario1/mod.rs
@@ -4,22 +4,50 @@ use bevy::prelude::*;
 
 use crate::{loading::TextureAssets, GameState};
 
-use self::components::PlanetEvacuationVessel;
+use self::components::{PlanetEvacuationVessel, SurfaceSceneRoot};
 
 pub struct Scenario1Plugin;
 
 impl Plugin for Scenario1Plugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(GameState::Playing), spawn_pev);
+        app.add_systems(OnEnter(GameState::Playing), spawn_scene);
+        app.add_systems(OnExit(GameState::Playing), clear_scene);
     }
 }
 
-fn spawn_pev(mut commands: Commands, textures: Res<TextureAssets>) {
+fn clear_scene(mut commands: Commands, scenes: Query<Entity, With<SurfaceSceneRoot>>) {
+    for entity in scenes.iter() {
+        commands.entity(entity).despawn_recursive();
+    }
+}
+
+fn spawn_scene(mut commands: Commands, textures: Res<TextureAssets>) {
     commands
-        .spawn(SpriteBundle {
-            texture: textures.texture_pev.clone(),
-            transform: Transform::from_translation(Vec3::new(40., 1000., 0.)),
-            ..Default::default()
-        })
-        .insert(PlanetEvacuationVessel);
+        .spawn(SpatialBundle::default())
+        .insert(SurfaceSceneRoot)
+        .with_children(|parent| {
+            parent
+                .spawn(SpriteBundle {
+                    texture: textures.texture_pev.clone(),
+                    transform: Transform::from_translation(Vec3::new(40., 1000., 0.)),
+                    ..Default::default()
+                })
+                .insert(PlanetEvacuationVessel);
+
+            parent
+                .spawn(SpriteBundle {
+                    texture: textures.texture_pev.clone(),
+                    transform: Transform::from_translation(Vec3::new(30., 500., 0.)),
+                    ..Default::default()
+                })
+                .insert(PlanetEvacuationVessel);
+
+            parent
+                .spawn(SpriteBundle {
+                    texture: textures.texture_pev.clone(),
+                    transform: Transform::from_translation(Vec3::new(80., 350., 0.)),
+                    ..Default::default()
+                })
+                .insert(PlanetEvacuationVessel);
+        });
 }
